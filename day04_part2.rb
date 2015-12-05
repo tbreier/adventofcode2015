0eda15732476938ee1c3ab21c5cca1a4d91c66fc
Thomas Breier <breierthomas@gmail.com>
2015-12-05 20:35:10 +0100

    brute forcing day 4

---
+++ day04_part2.rb
@@ -0,0 +1,16 @@
+require 'digest/md5'
+
+ARGF.each_line do |line|
+	print 'Working...'
+	line = line.strip
+	i = 0
+
+	loop do
+		if Digest::MD5.hexdigest("#{line}#{i}").start_with?('000000')
+			puts "\n#{i}"
+			break
+		end
+		i += 1
+		print "\rWorking... #{i}" if i % 10000 == 0
+	end
+end
