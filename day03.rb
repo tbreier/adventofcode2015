d10318b3d41908af60a893f842d43b79960010f4
Thomas Breier <breierthomas@gmail.com>
2015-12-05 19:51:54 +0100

    day 03 part 1

---
+++ day03.rb
@@ -0,0 +1,24 @@
+# Init houses & Start location
+houses = Hash.new { |hash, key| hash[key] = 0 }
+curr = [0,0]
+houses[curr] += 1
+
+ARGF.each_line do |line|
+	line.each_char do |direction|
+		case direction
+		when '>'
+			curr[0] += 1
+		when '<'
+			curr[0] -= 1
+		when '^'
+			curr[1] += 1
+		when 'v'
+			curr[1] -= 1
+		else
+			break # Done
+		end
+		houses[curr] += 1
+	end
+end
+
+puts houses.keys.length
