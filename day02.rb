3a89f586203cabd7fa49682a57e105e5712896d2
Thomas Breier <breierthomas@gmail.com>
2015-12-05 19:41:31 +0100

    day 2 part 2

--- day02.rb
+++ day02.rb
@@ -1,8 +1,11 @@
-total = 0
+paper = 0
+ribbon = 0
 
 ARGF.each_line do |line|
 	sides = line.split('x').map(&:to_i)
-	total += 2 * (sides[0] * sides[1] + sides[0] * sides[2] + sides[1] * sides[2]) + sides.inject(:*) / sides.max
+	paper += 2 * (sides[0] * sides[1] + sides[0] * sides[2] + sides[1] * sides[2]) + sides.inject(:*) / sides.max
+	ribbon += sides.inject(:*) + 2 * (sides.inject(:+) - sides.max)
 end
 
-puts total
+puts "Total paper needed: #{paper} square feet"
+puts "Total ribbon needed: #{ribbon} feet"
